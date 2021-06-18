8de70687bad082004d4d96152541ed199929af1d
Abhisek Datta <abhisek.datta@cern.ch>
2021-06-17 17:47:39 -0700

    fix typo

--- lpgbt_vfat_daq_scurve.py
+++ lpgbt_vfat_daq_scurve.py
@@ -181,9 +181,11 @@ def lpgbt_vfat_scurve(system, vfat_list, step, nl1a, l1a_bxgap):
         configureVfat(0, vfat-6*oh_select, oh_select, 0)
 
     # Writing Results
-    for vfat in daq_data:
-        for channel in daq_data[vfat]:
-            for charge in daq_data[vfat][channel]:
+    for vfat in vfat_list:
+        for channel in range(0,128):
+            for charge in range(0,256,1):
+                if charge not in daq_data[vfat][channel]:
+                    continue
                 file_out.write("%d    %d    %d    %d    %d\n"%(vfat, channel, charge, daq_data[vfat][channel][charge]["fired"], daq_data[vfat][channel][charge]["events"]))
 
     print ("")
